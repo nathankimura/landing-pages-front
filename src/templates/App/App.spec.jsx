93f7e74fd31f22a48538727659ea889174795064
nathan <nathankimura14@gmail.com>
2023-04-03 09:44:58 -0300

    feat: create tests for the Heading component,  testing its styles too

--- src/templates/App/App.spec.jsx
+++ src/templates/App/App.spec.jsx
@@ -3,6 +3,9 @@ import Home from '.';
 import { renderTheme } from '../../styles/render-theme';
 import { theme } from '../../styles/theme';
 
-test('renders Home', () => {
-  renderTheme(<Home />);
-});
+
+describe('<Home />', () => {
+  it('should render Home', () => {
+    renderTheme(<Home />);
+  })
+})
