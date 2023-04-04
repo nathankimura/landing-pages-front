e2a2c706c48974db99272428f17a8c72c2475b4a
nathan <nathankimura14@gmail.com>
2023-04-04 09:11:23 -0300

    feat: create component logoLink and all files related to it,  including its tests

--- src/components/LogoLink/LogoLink.spec.jsx
+++ src/components/LogoLink/LogoLink.spec.jsx
@@ -3,8 +3,21 @@ import { LogoLink } from '.';
 import { renderTheme } from '../../styles/render-theme'
 
 describe('<LogoLink />', () => {
-  it('should render', () => {
-    renderTheme(<LogoLink>Children</LogoLink>);
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+  it('should render text logo', () => {
+    renderTheme(<LogoLink link='#target' text='Olá mundo' />);
+    expect(screen.getByRole('heading', { name: 'Olá mundo' })).toBeInTheDocument();
+    const link = screen.getByRole('link', { name: 'Olá mundo' });
+    expect(link).toHaveAttribute('href', '#target');
+    // screen.debug(heading);
+  });
+
+  it('should render image logo', () => {
+    renderTheme(<LogoLink link='#target' text='Olá mundo' srcImg='image.svg' />);
+    expect(screen.getByRole('img', { name: 'Olá mundo' })).toHaveAttribute('src', 'image.svg');
+  });
+
+  it('should match snapshot', () => {
+    const { container } = renderTheme(<LogoLink link='#target' text='Olá mundo' srcImg='image.svg' />);
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
--- src/components/LogoLink/styles.js
+++ src/components/LogoLink/styles.js
@@ -1,7 +1,14 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.a`
   ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: inherit;
 
+    > img {
+      max-height: 3rem;
+    }
   `}
 `;
