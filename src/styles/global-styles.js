2e6a143c443124a1efab7e8470e2bc72e0172d03
nathan <nathankimura14@gmail.com>
2023-04-05 10:46:55 -0300

    fix: correct some styles at globalStyles file, and corrct import at TextComponent so it can be used receiving html

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -13,10 +13,24 @@ export const GlobalStyles = createGlobalStyle`
 
   body {
     font-size: 1.6rem;
-    font-family: 'Open Sans', sans-serif;
+    font-family: ${({ theme }) => theme.font.family.default};
   }
 
   h1, h2, h3, h4, h5, h6 {
-    font-family: 'Montserrat', sans-serif;
+    font-family: ${({ theme }) => theme.font.family.secondary};
+    margin: ${({ theme }) => theme.spacings.large} 0;
+  }
+
+  p {
+    margin: ${({ theme }) => theme.spacings.medium} 0;
+  }
+
+  ul, ol {
+    margin: ${({ theme }) => theme.spacings.medium} 0;
+    padding: ${({ theme }) => theme.spacings.medium} 0;
+  }
+
+  a {
+    color: ${({ theme }) => theme.colors.secondaryColor};
   }
 `;
