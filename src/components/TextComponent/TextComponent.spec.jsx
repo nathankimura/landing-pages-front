237106c6e7ad5300455b21dc07315656520b4a41
nathan <nathankimura14@gmail.com>
2023-05-04 15:30:51 -0300

    fix: att snapshots related to the creation of GridImage

--- src/components/TextComponent/TextComponent.spec.jsx
+++ src/components/TextComponent/TextComponent.spec.jsx
@@ -24,11 +24,11 @@ describe('<TextComponent />', () => {
         font-size: 2.4rem;
       }
 
-      <p
+      <div
         class="c0"
       >
         Children
-      </p>
+      </div>
     `);
   });
 });
