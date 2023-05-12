9fd44668ad1cc34b3e79ed2c45a62f32adc7b661
nathan <nathankimura14@gmail.com>
2023-05-12 16:48:41 -0300

    feat: add browser router

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyles } from './styles/global-styles';
 import { theme } from './styles/theme';
-import Home from './templates/App';
+import Home from './templates/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path='*' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
       <GlobalStyles />
     </ThemeProvider>
   </React.StrictMode>,
